9b21cf44938913c5a7877eb661c89903cb65a09b
860119012 <860119012@DESKTOP-ES5PICB.fcn.local>
2019-08-01 10:26:47 +0800

    20190801.001

---
+++ demo_source/src/main/java/kamiSaman/kami/demo_source/App.java
@@ -0,0 +1,13 @@
+package kamiSaman.kami.demo_source;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
